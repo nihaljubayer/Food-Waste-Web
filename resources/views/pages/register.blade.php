06913b164c19a7fc4f9001da5648beff39a1bb2f
Rafiya Tasnim Hossain <hossain22205101005@diu.edu.bd>
2025-11-20 14:49:33 +0600

    Create register.blade.php

---
+++ resources/views/pages/register.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>this is a registration form</h2>
+    <form>
+      name:  <input type="text" placeholder="emter your name">
+    </form>
+
+</body>
+</html>
