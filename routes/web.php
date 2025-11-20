c7f12eb982ac184b5f87a25d4b7871cdcdded3c0
nihaljubayer <nihaljubayer777@gmail.com>
2025-11-21 00:48:58 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@ Route::get('/signup', function () {
 Route::get('/register/donor', [AuthController::class, 'showDonorRegisterForm'])
     ->name('register.donor');
 
+Route::get('/register/organization', [AuthController::class, 'showOrganizationRegisterForm'])
+    ->name('register.organization');
+
+Route::post('/register', [AuthController::class, 'register'])
+    ->name('register.post');
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login.post');
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+Route::get('/dashboard', [AuthController::class, 'dashboard'])
+    ->middleware('auth')
+    ->name('dashboard');
