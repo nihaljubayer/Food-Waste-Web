eaf20d23f7be474d9dcacb2a72c3ebfc9a70de03
nihaljubayer <nihaljubayer777@gmail.com>
2025-11-21 00:46:09 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+Route::get('/', function () {
+    return view('pages.home');
+})->name('home');
+
+Route::get('/signup', function () {
+    return view('pages.register');   // registration choice page
+})->name('signup.choice');
+
+Route::get('/register/donor', [AuthController::class, 'showDonorRegisterForm'])
+    ->name('register.donor');
+
