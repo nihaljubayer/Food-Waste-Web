1ebf0d6a0de017695d7fc99f0f1e84e06f9c25f3
nihaljubayer <nihaljubayer777@gmail.com>
2025-11-21 15:18:38 +0600

    Registration Form Methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,3 +6,15 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+ public function showDonorRegisterForm()
+ {
+ return view('Auth.register_donor');
+ }
+
+ public function showOrganizationRegisterForm()
+ {
+ return view('Auth.register_organization');
+ }
