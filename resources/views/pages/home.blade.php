7d2cc869634cfa4328f426fd7f9e6c26a14db7d3
siamahmed0011 <ahmedsiam01608@gmail.com>
2025-11-21 13:04:48 +0600

    updated routes and home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,7 @@
     .hero-section{
         width:100%;
         min-height:90vh;
-        background:url('{{ asset('images/home1.jpg') }}') center center/cover no-repeat;
+        background:url('{{ asset('images/h2.jpg') }}') center center/cover no-repeat;
         position:relative;
         color:#fff;
     }
@@ -88,7 +88,7 @@
     }
 </style>
 
-
+{{-- ================= HERO SECTION ================= --}}
 <section class="hero-section">
     <div class="hero-overlay"></div>
 
@@ -225,23 +225,8 @@
 </section>
 
 {{-- ================= STATS BAND ================= --}}
-<section class="stats-band py-4">
-    <div class="container">
-        <div class="row text-center">
-            <div class="col-md-4 mb-3 mb-md-0">
-                <h3>100K+</h3>
-                <p class="mb-0">Meals Saved from Waste</p>
-            </div>
-            <div class="col-md-4 mb-3 mb-md-0">
-                <h3>250+</h3>
-                <p class="mb-0">Registered Donors</p>
-            </div>
-            <div class="col-md-4">
-                <h3>80+</h3>
-                <p class="mb-0">Partner NGOs & Food Banks</p>
-            </div>
-        </div>
-    </div>
+<section class="stats-band py-3">
+  
 </section>
 
 {{-- ================= FOR DONORS & ORGS ================= --}}
