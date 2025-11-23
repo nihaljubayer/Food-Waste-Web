9de66247cd95f98dc4cd2c8622e6c0c56e666ba9
nihaljubayer <nihaljubayer777@gmail.com>
2025-11-23 10:45:04 +0600

    folder update

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
-Route::get('/test',function(){
+
+Route::get('/', function () {
     return view('main');
 });
