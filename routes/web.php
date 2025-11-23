0252d96de948423829b5a80d607927e3f3ae2aab
nihaljubayer <nihaljubayer777@gmail.com>
2025-11-23 10:47:24 +0600

    web.php code update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('main');
-});
+    return view('pages.home');
+})->name('home');
+
+Route::get('/signup', function () {
+    return view('pages.register');   // registration choice page
+})->name('signup.choice');
+
+Route::get('/register/donor', [AuthController::class, 'showDonorRegisterForm'])
+    ->name('register.donor');
+
+Route::get('/register/organization', [AuthController::class, 'showOrganizationRegisterForm'])
+    ->name('register.organization');
+
+Route::post('/register', [AuthController::class, 'register'])
+    ->name('register.post');
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login.post');
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+Route::get('/dashboard', [AuthController::class, 'dashboard'])
+    ->middleware('auth')
+    ->name('dashboard');
