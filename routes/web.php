a87956e1f0494110f94a92bdd320a5f120f95e13
nihaljubayer <nihaljubayer777@gmail.com>
2025-11-23 10:22:43 +0600

    Adding the necessary file folders for the project

--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,32 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// Home page
+
 Route::get('/', function () {
     return view('pages.home');
 })->name('home');
 
-// Signup Choice Page
+
 Route::get('/signup', function () {
-    return view('pages.register');   // যেখানে donor/organization choose page আছে
+    return view('pages.register');  
 })->name('signup.choice');
 
-// Donor Register Form
+
 Route::get('/register/donor', [AuthController::class, 'showDonorRegisterForm'])
     ->name('register.donor');
 
-// Organization Register Form
+
 Route::get('/register/organization', [AuthController::class, 'showOrganizationRegisterForm'])
     ->name('register.organization');
 
-// Registration POST
+
 Route::post('/register', [AuthController::class, 'register'])->name('register.post');
 
-// Login Page
+
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 
-// Login POST
+
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
-// Dashboard
+
 Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth')->name('dashboard');
